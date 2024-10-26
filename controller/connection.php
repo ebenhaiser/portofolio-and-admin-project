0abea6dd42f159c127c9c9f033ad18aca369a81e
Ebenhaiser Caprisiano <ebenhaiser@gmail.com>
2024-10-26 15:01:08 +0700

    edit

---
+++ controller/connection.php
@@ -0,0 +1,9 @@
+<?php
+    $connectionHost = 'localhost';
+    $connectionUsername = 'root';
+    $connectionPassword =  '';
+    $connectionDatabase = 'angkatan3_portofolio';
+
+    $connection = mysqli_connect($connectionHost, $connectionUsername, $connectionPassword, $connectionDatabase);
+
+?>
---
+++ controller/query.php
@@ -0,0 +1,5 @@
+<?php
+    $queryPortofolio = mysqli_query($connection, "SELECT * FROM portofolio ORDER BY id DESC");
+
+    
+?>
