8edbfb8664e054ec2852b603cd739baef10a568e
Nama Kamu <ebenhaiser@gmail.com>
2024-10-31 08:58:58 +0700

    edit

--- angkatan3_portofolio.sql
+++ angkatan3_portofolio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 31, 2024 at 02:43 AM
+-- Generation Time: Oct 31, 2024 at 02:58 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -487,7 +487,7 @@ ALTER TABLE `portofolio_hero`
 -- AUTO_INCREMENT for table `portofolio_message`
 --
 ALTER TABLE `portofolio_message`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `portofolio_project`
